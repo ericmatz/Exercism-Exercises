a2eefc04e33e2dc730aa628fe3e7ecb7ab21b627
= <ericmatz1@gmail.com>
2020-09-21 17:12:35 -0500

    Updated Solution for raindrops[Bash]

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 main(){
-    if (( $1 % 3  != 0 )) && (( $1 % 5 != 0 )) && (( $1 % 7 != 0 )); then
-        echo $1
-    else
-        if (( $1 % 3 == 0 )); then
-            output+="Pling"
-        fi
-        if (( $1 % 5 == 0 )); then
-            output+="Plang"
-        fi
-        if (( $1 % 7 == 0 )); then
-            output+="Plong"
-        fi
-        echo ${output:-$1}
+    if (( $1 % 3 == 0 )); then
+        output+="Pling"
     fi
+    if (( $1 % 5 == 0 )); then
+        output+="Plang"
+    fi
+    if (( $1 % 7 == 0 )); then
+        output+="Plong"
+    fi
+    echo ${output:-$1}
 }
 
 main "$@"
