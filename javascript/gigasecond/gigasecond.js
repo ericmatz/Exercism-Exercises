d3da13c2a2864ca394cbfb52fd354b9ef78f594b
= <ericmatz1@gmail.com>
2020-09-21 17:09:36 -0500

    Finished gigasecond[JavaScript]

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,11 +1,6 @@
-import { Console } from "console";
 
 export const gigasecond = (date) => {
-  // const gs = gigasecond(new Date(Date.UTC(2011, 3, 25)));
-  // const expectedDate = new Date(Date.parse('2043-01-01T01:46:40Z'));
-  // console.log
-  // 2011-04-25T00:00:00.000Z
-  date.setUTCSeconds(date.getUTCSeconds() + 10e9)
-  console.log(date)
-  return 
+  var newDate = new Date(Date.parse(date.toUTCString()))
+  newDate.setUTCSeconds(newDate.getUTCSeconds() + 1000000000)
+  return newDate
 };
