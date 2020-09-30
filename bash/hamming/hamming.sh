83adeeb235badc479ea84e4bf4293e87bf81a1a3
Eric Matz <ericmatz1@gmail.com>
2020-09-30 18:04:50 -0500

    Updated Solution to hamming [bash]

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -3,17 +3,20 @@
 main(){
     if (( "$#" != 2 )); then
         echo "Usage: hamming.sh <string1> <string2>"
-        exit 1
+        return 1
     elif (( ${#1} != ${#2} )); then
         echo "left and right strands must be of equal length"
+        return 1
     else
         for ((i = 0 ; i < ${#1} ; i++)); do
             if [[ "${1:$i:1}" != "${2:$i:1}" ]]; then
                 ((count++))
             fi
         done
-        echo ${count:-0}
+        echo "${count:-0}"
     fi
 }
 
 main "$@"
+
+exit $?
